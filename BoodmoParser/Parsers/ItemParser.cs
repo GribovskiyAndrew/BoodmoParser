b60fece191b15f8bce8abf8ff86811039ae534b7
GribovskiyAndrew <87425788+GribovskiyAndrew@users.noreply.github.com>
2022-09-07 19:42:41 +0300

    some changes

---
+++ BoodmoParser/Parsers/ItemParser.cs
@@ -0,0 +1,36 @@
+﻿using BoodmoParser.Database.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoodmoParser.Parsers
+{
+    public class ItemParser : Parser
+    {
+        public ItemParser(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public async Task<string> Get(string url, HttpClient httpClient)
+        {
+            while (true)
+                try
+                {
+                    return await httpClient.GetStringAsync(url);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+        }
+
+        public override async Task Parse()
+        {
+
+
+        }
+
+    }
+}
